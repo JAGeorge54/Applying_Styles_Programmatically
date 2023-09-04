66138e07f02244ec467f1bc25a6e541be1d5f4ef
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-09-04 08:44:33 -0400

    added off function

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function on(){
     const element = document.getElementById('target');
     element.classList.add('on');
 }
+function off(){
+    const element = document.getElementById('target');
+    element.classList.remove('on');
+}
